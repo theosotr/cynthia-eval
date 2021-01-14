11643c8eb632624198064f6d08c1f5cbb6ab7182
Thodoris Sotiropoulos <theosotr@aueb.gr>
2021-01-14 12:29:29 +0200

    Add more test cases

---
+++ bugs/test-cases/9/TestSchema.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS "t1";
+CREATE TABLE "t1" (
+"id" integer,
+"col" numeric(10, 2),
+PRIMARY KEY (id)
+);
+
+INSERT INTO "t1" ("id", "col") VALUES (1, 43.5);
+INSERT INTO "t1" ("id", "col") VALUES (2, 12.1);
+INSERT INTO "t1" ("id", "col") VALUES (3, 9.33);
+INSERT INTO "t1" ("id", "col") VALUES (4, 3.1);
+INSERT INTO "t1" ("id", "col") VALUES (5, 4.46);
+INSERT INTO "t1" ("id", "col") VALUES (6, 23.5);
