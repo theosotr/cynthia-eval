50235c9450bb0b902e060d514117be2187d59297
Thodoris Sotiropoulos <theosotr@aueb.gr>
2021-01-14 20:51:19 +0200

    Make duplicate values

--- bugs/test-cases/15/TestSchema.sql
+++ bugs/test-cases/15/TestSchema.sql
@@ -5,9 +5,9 @@ CREATE TABLE "t1" (
 PRIMARY KEY (id)
 );
 
-INSERT INTO "t1" ("id", "col") VALUES (1, 43.5);
-INSERT INTO "t1" ("id", "col") VALUES (2, 12.1);
-INSERT INTO "t1" ("id", "col") VALUES (3, 9.33);
+INSERT INTO "t1" ("id", "col") VALUES (1, 3.1);
+INSERT INTO "t1" ("id", "col") VALUES (2, 3.1);
+INSERT INTO "t1" ("id", "col") VALUES (3, 3.1);
 INSERT INTO "t1" ("id", "col") VALUES (4, 3.1);
 INSERT INTO "t1" ("id", "col") VALUES (5, 4.46);
 INSERT INTO "t1" ("id", "col") VALUES (6, 23.5);
